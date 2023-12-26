f441dce266d9d75284d2ef352f111f796d2d4e45
JavadAlip <javadalipp911@gmail.com>
2023-12-26 10:08:33 -0800

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
 const express=require('express')
 const app=express()
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const authRoute=require('./routes/auth.js')
 
 // database
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("database connected successesfully")
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+// middleware
+dotenv.config()
+app.use(express.json())
+app.use('/api/auth',authRoute)
 app.listen(5000,()=>{
+    connectDB()
+
     console.log("app is running on port 5000")
 })
