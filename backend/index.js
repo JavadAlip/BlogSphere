9b366c47d9715ad367beb38b53ac2c8e6af8121e
JavadAlip <javadalipp911@gmail.com>
2023-12-30 11:12:44 -0800

    Register process completed

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,12 @@ const express=require('express')
 const app=express()
 const mongoose=require('mongoose')
 const dotenv=require('dotenv')
+const cors=require('cors')
+const cookieParser=require('cookie-parser')
 const authRoute=require('./routes/auth.js')
+const userRoute=require('./routes/user.js')
+const postRoute=require('./routes/post.js')
+const commentRoute=require('./routes/comment.js')
 
 // database
 const connectDB=async()=>{
@@ -14,13 +19,18 @@ const connectDB=async()=>{
         console.log(err)
     }
 }
+dotenv.config()
 
 // middleware
 dotenv.config()
 app.use(express.json())
+app.use(cookieParser())
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use('/api/auth',authRoute)
+app.use('/api/users',userRoute)
+app.use('/api/posts',postRoute)
+app.use('/api/comments',commentRoute)
 app.listen(5000,()=>{
     connectDB()
-
     console.log("app is running on port 5000")
 })
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer';
+import axios from 'axios'
+import { URL } from '../url.js';
 
 const Register = () => {
+  const [username, setUsername]=useState("")
+  const [email, setEmail]=useState("")
+  const [password, setPassword]=useState("")
+  const [error,setError]=useState("")
+  const navigate=useNavigate()
+const handleRegister=async()=>{
+  try {
+    const res= await axios.post(URL+"/api/auth/register",{username,email,password})
+    setUsername(res.data.username)
+    setEmail(res.data.email)
+    setPassword(res.data.password)
+    setError(false)
+    navigate("/login")
+
+  } catch (err) {
+    setError(true)
+    console.log(err)
+  }
+}
   return (
     <>
     <div className='flex items-center justify-between px-6 md:px-[200px] py-4'>
@@ -12,10 +33,11 @@ const Register = () => {
     <div className='w-full flex justify-center items-center h-[80vh] '>
     <div className='flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]'>
       <h1 className='text-xl font-bold text-left'>Create an account</h1>
-      <input className='w-full px-4 py-4 rounded-full border-2 border-black outline-0' type='text' placeholder='Username'/>
-      <input className='w-full px-4 py-4 rounded-full border-2 border-black outline-0' type='text' placeholder='E-Mail'/>
-      <input className='w-full px-4 py-4 rounded-full border-2 border-black outline-0' type='text' placeholder='Password'/>
-      <button className='w-full px-4 py-4 font-bold text-white hover:bg-gray-500 hover:text-black bg-black rounded-full'>Register</button>
+      <input onChange={(e)=>setUsername(e.target.value)} className='w-full px-4 py-4 rounded-full border-2 border-black outline-0' type='text' placeholder='Username'/>
+      <input onChange={(e)=>setEmail(e.target.value)} className='w-full px-4 py-4 rounded-full border-2 border-black outline-0' type='text' placeholder='E-Mail'/>
+      <input onChange={(e)=>setPassword(e.target.value)} className='w-full px-4 py-4 rounded-full border-2 border-black outline-0' type='text' placeholder='Password'/>
+      <button onClick={handleRegister} className='w-full px-4 py-4 font-bold text-white hover:bg-gray-500 hover:text-black bg-black rounded-full'>Register</button>
+      {error && <h3 className='text-red-500 text-sm'>Try another details!</h3>}
       <div className='flex justify-center items-center space-x-2'>
         <p>Already have an account?</p>
         <p className='text-gray-500 hover:text-black'><Link to="/login">Login</Link></p>
