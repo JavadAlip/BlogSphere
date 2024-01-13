dea6337ea9baf5fa20c08605d2053f0f62eaac71
JavadAlip <javadalipp911@gmail.com>
2024-01-13 11:56:10 +0530

    change verityToekn name little bit

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Comment = require('../models/Comment');
-const verifyToken = require('../verifyToken');
+const verifyToken = require('../verifyToekn');
 
 // create
 router.post("/create", verifyToken, async (req, res) => {
