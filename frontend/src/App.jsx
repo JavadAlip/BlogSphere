ad047ea32e7454b460e9314d9b88c7a542b72fb5
JavadAlip <javadalipp911@gmail.com>
2024-01-13 11:33:38 +0530

    app.jsx and usercontext updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,15 @@ import PostDetails from './pages/PostDetails'
 import CreatePost from './pages/CreatePost'
 import EditPost from './pages/EditPost'
 import Profile from './pages/Profile'
+import { UserContextProvider } from './context/UserContext.jsx'
+import MyBlogs from './pages/MyBlogs.jsx'
+// Example in App.js or your main layout component
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
   return (
-    <div>
+    <UserContextProvider>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
@@ -18,10 +24,12 @@ const App = () => {
         <Route exact path="/posts/post/:id" element={<PostDetails />} />
         <Route exact path="/create" element={<CreatePost />} />
         <Route exact path="/edit/:id" element={<EditPost />} />
+        <Route exact path="/myblogs/:id" element={<MyBlogs />} />
         <Route exact path="/profile/:id" element={<Profile />} />
         <Route exact path="/comment" element={<Comment />} />
-        </Routes>
-    </div>
+      </Routes>
+      <ToastContainer />
+      </UserContextProvider>
   )
 }
 export default App
--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,58 +1,9 @@
-// import {  createContext, useEffect, useState } from "react";
-// import axios from "axios";
-// import { URL } from "../url";
-
-// export const UserContext=createContext({})
-// export function UserContextProvider({children}){
-//     const [user,setUser]=useState(null)
-//     useEffect(()=>{
-//         getUser()
-//     },[])
-    
-//     // const getUser=async()=>{
-//     //     try {
-//     //         const res=await axios.get(URL+"/api/auth/refetch",{withCredentials:true})
-
-//     //         // const res = await axios({
-//     //         //     method: "get",
-//     //         //     baseURL: `${URL}/api/auth`, // Using the URL constant from url.js
-//     //         //     url: "refetch",
-//     //         //     withCredentials: true,
-//     //         //   });
-            
-//     //         console.log(res.data)
-//     //         setUser(res.data)
-//     //     } catch (err) {
-//     //         console.error(err,"Axios error anne")
-//     //     }
-//     // }
-
-//     const getUser = async () => {
-//         try {
-//             const res = await axios.get(URL + "/api/auth/refetch", { withCredentials: true });
-//             console.log("Response from getUser:", res.data);
-//             setUser(res.data);
-//         } catch (err) {
-//             console.error("Error in getUser:", err);
-//         }
-//     };
-    
-//     return <UserContext.Provider value={{user,setUser}} >
-//     {children}
-//     </UserContext.Provider>
-// }
-
-
-
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import { URL } from "../url";
-
 export const UserContext = createContext({});
-
 export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
-
     useEffect(() => {
         getUser();
     }, []);
