f9700cc24756c9ca75af651550d9ded98b8a5c57
JavadAlip <javadalipp911@gmail.com>
2024-01-14 16:24:40 +0530

    insatll bycrypt

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Post=require('../models/Post');
 const verifyToken = require('../verifyToekn');
 const Comment = require('../models/Comment');
