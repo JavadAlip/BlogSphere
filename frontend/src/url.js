fcca112743762bc6a54ac4469b560c00c1eef6c9
JavadAlip <javadalipp911@gmail.com>
2024-01-14 23:49:27 +0530

    all thinks done

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,5 +1,5 @@
 // export const URL="https://blogsphere-mr0r.onrender.com"
 // export const IMGFOLDER="https://blogsphere-mr0r.onrender.com/images/"
 
-export const URL="https://blogsphere-mr0r.onrender.com"
+export const URL="http://localhost:10000"
 export const IMGFOLDER="http://localhost:10000/images/"
