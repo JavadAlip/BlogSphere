044b63f151e72d3179ac02a3512d6c498865682c
JavadAlip <javadalipp911@gmail.com>
2024-01-14 23:20:13 +0530

    last

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -2,4 +2,4 @@
 // export const IMGFOLDER="https://blogsphere-mr0r.onrender.com/images/"
 
 export const URL="https://blogsphere-mr0r.onrender.com"
-export const IMGFOLDER="https://blogsphere-mr0r.onrender.com/images/"
+export const IMGFOLDER="http://localhost:10000/images/"
