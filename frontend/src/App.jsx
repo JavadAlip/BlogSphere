581e8fc53e03f3e3044d07712d5659abe3d31f7c
JavadAlip <javadalipp911@gmail.com>
2024-01-14 19:31:58 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import './app.css'
-import React from 'react'
-import Home from './pages/Home'
-import { Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import PostDetails from './pages/PostDetails'
-import CreatePost from './pages/CreatePost'
-import EditPost from './pages/EditPost'
-import Profile from './pages/Profile'
-import { UserContextProvider } from './context/UserContext.jsx'
-import MyBlogs from './pages/MyBlogs.jsx'
-// Example in App.js or your main layout component
+import './app.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PostDetails from './pages/PostDetails';
+import CreatePost from './pages/CreatePost';
+import EditPost from './pages/EditPost';
+import Profile from './pages/Profile';
+import MyBlogs from './pages/MyBlogs';
+
+import { UserContextProvider } from './context/UserContext';
 
 const App = () => {
   return (
     <UserContextProvider>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/posts/post/:id" element={<PostDetails />} />
-        <Route exact path="/create" element={<CreatePost />} />
-        <Route exact path="/edit/:id" element={<EditPost />} />
-        <Route exact path="/myblogs/:id" element={<MyBlogs />} />
-        <Route exact path="/profile/:id" element={<Profile />} />
-        <Route exact path="/comment" element={<Comment />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/posts/post/:id" element={<PostDetails />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+        <Route path="/myblogs/:id" element={<MyBlogs />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/comment" element={<Comment />} />
       </Routes>
       <ToastContainer />
-      </UserContextProvider>
-  )
-}
-export default App
+    </UserContextProvider>
+  );
+};
+export default App;
