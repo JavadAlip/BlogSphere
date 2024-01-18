4c3925b365fe96e20645b7c778eee0f2b476eff5
JavadAlip <javadalipp911@gmail.com>
2024-01-18 13:34:08 +0530

    final change

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,16 +1,48 @@
+// import React, { createContext, useEffect, useState } from "react";
+// import axios from "axios";
+// import { URL } from "../url.js";
+// export const UserContext = createContext({});
+// export function UserContextProvider({ children }) {
+//     const [user, setUser] = useState(null);
+//     useEffect(() => {
+//         getUser();
+//     }, []);
+
+//     const getUser = async () => {
+//         try {
+//             const res = await axios.get(URL + "/api/auth/refetch", { withCredentials: true });
+//             console.log("Response from getUser:", res.data);
+//             setUser(res.data);
+//         } catch (err) {
+//             console.error("Error in getUser:", err);
+//         }
+//     };
+
+//     return (
+//         <UserContext.Provider value={{ user, setUser }}>
+//             {children}
+//         </UserContext.Provider>
+//     );
+// }
+
+
+
 import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import { URL } from "../url.js";
+
 export const UserContext = createContext({});
+
 export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
+
     useEffect(() => {
         getUser();
     }, []);
 
     const getUser = async () => {
         try {
-            const res = await axios.get(URL + "/api/auth/refetch", { withCredentials: true });
+            const res = await axios.get(`${URL}/api/auth/refetch`, { withCredentials: true });
             console.log("Response from getUser:", res.data);
             setUser(res.data);
         } catch (err) {
@@ -24,4 +56,3 @@ export function UserContextProvider({ children }) {
         </UserContext.Provider>
     );
 }
-
