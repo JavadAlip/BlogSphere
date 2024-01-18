65727a395eaf8d79f906122603ddc1f976324c0c
JavadAlip <javadalipp911@gmail.com>
2024-01-19 01:24:39 +0530

    remove url.js and add VITE_URL in frontnend.env

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,6 +1,6 @@
-// export const URL = process.env.REACT_APP_BACKEND_URL || "https://blogsphere-o6fp.onrender.com";
-// export const IMGFOLDER = process.env.REACT_APP_IMG_FOLDER || "https://blogsphere-o6fp.onrender.com/images/";
+// export const q = "http://localhost:10000";
+// export const pa =  "http://localhost:10000/images/";
+// url.js
+// export const q = process.env.REACT_APP_BACKEND_URL || "http://localhost:10000";
+// export const pa = process.env.REACT_APP_IMG_FOLDER || "http://localhost:10000/images/";
 
-
-export const q = process.env.REACT_APP_BACKEND_URL || "https://blogsphere-o6fp.onrender.com";
-export const pa = process.env.REACT_APP_IMG_FOLDER || "https://blogsphere-o6fp.onrender.com/images/";
