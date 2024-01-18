d354aa22eb419ed1338f09fc874efc5c21f1d05e
JavadAlip <javadalipp911@gmail.com>
2024-01-18 12:49:09 +0530

    change userContext

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,27 +1,59 @@
+// import React, { createContext, useEffect, useState } from "react";
+// import axios from "axios";
+// import { URL } from "../url";
+// export const UserContext = createContext({});
+// export function UserContextProvider({ children }) {
+//     const [user, setUser] = useState(null);
+//     useEffect(() => {
+//         getUser();
+//     }, []);
+
+//     const getUser = async () => {
+//         try {
+//             const res = await axios.get(URL + "/api/auth/refetch", { withCredentials: true });
+//             console.log("Response from getUser:", res.data);
+//             setUser(res.data);
+//         } catch (err) {
+//             console.error("Error in getUser:", err);
+//         }
+//     };
+
+//     return (
+//         <UserContext.Provider value={{ user, setUser }}>
+//             {children}
+//         </UserContext.Provider>
+//     );
+// }
+
 import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import { URL } from "../url";
+
 export const UserContext = createContext({});
+
 export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         getUser();
     }, []);
 
     const getUser = async () => {
         try {
-            const res = await axios.get(URL + "/api/auth/refetch", { withCredentials: true });
+            const res = await axios.get(`${URL}/api/auth/refetch`, { withCredentials: true });
             console.log("Response from getUser:", res.data);
             setUser(res.data);
         } catch (err) {
             console.error("Error in getUser:", err);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, setUser, loading }}>
             {children}
         </UserContext.Provider>
     );
 }
-
