b0b8b17e5c33417b3c06bae3efaa3eeb140c26c9
JavadAlip <javadalipp911@gmail.com>
2024-01-19 15:15:37 +0530

    origin change

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use('/images',express.static(path.join(__dirname,"/images")))
 
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin:"https://blog-sphere-lac.vercel.app", credentials: true }));
 
 
 // Database
