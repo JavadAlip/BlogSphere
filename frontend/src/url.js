7f0114dff34a9063f91c6a36b2be4c1457e44a6a
JavadAlip <javadalipp911@gmail.com>
2024-01-24 00:29:04 +0530

    url add

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,6 +1,2 @@
-// export const q = "http://localhost:10000";
-// export const pa =  "http://localhost:10000/images/";
-// url.js
-// export const q = process.env.REACT_APP_BACKEND_URL || "http://localhost:10000";
-// export const pa = process.env.REACT_APP_IMG_FOLDER || "http://localhost:10000/images/";
-
+export const VITE_URL="https://blogsphere-backend.onrender.com"
+export const VITE_IMGFOLDER="https://blogsphere-backend.onrender.com/images/"
