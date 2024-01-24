97cae3718d266e6eb125a58f63b11a261af49ba3
JavadAlip <javadalipp911@gmail.com>
2024-01-24 11:02:14 +0530

    today

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,4 @@
 export const VITE_URL="https://blogsphere-backend.onrender.com"
-export const VITE_IMGFOLDER="https://blogsphere-backend.onrender.com/images/"
+// export const VITE_IMGFOLDER="https://blogsphere-backend.onrender.com/images/"
+
+export const VITE_IMGFOLDER="http://localhost:10000/images/"
