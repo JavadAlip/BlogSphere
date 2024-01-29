72f2b900569002b2ea0fdceb30c2a5880a498bec
JavadAlip <javadalipp911@gmail.com>
2024-01-30 00:37:04 +0530

    homepost updated

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -243,7 +243,7 @@ const CreatePost = () => {
             {
               method: 'post',
               body: data,
-            }
+            } 
           );
 
           if (!cloudinaryResponse.ok) {
