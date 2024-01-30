c0e2ef6896d09cdc9d66729c8438cf51dc748ef5
JavadAlip <javadalipp911@gmail.com>
2024-01-30 19:31:45 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -8,19 +8,36 @@ const authRoute = require('./routes/auth.js');
 const userRoute = require('./routes/user.js');
 const postRoute = require('./routes/post.js');
 const commentRoute = require('./routes/comment.js');
-const multer=require('multer')
-const path=require("path")
-
+const multer = require('multer');
+const path = require('path');
 
 // Middleware
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.use('/images',express.static(path.join(__dirname,"/images")))
 
+// Multer image upload
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.img);
+        // cb(null, "imageme.jpg");
+    },
+});
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single("file"), (req, res) => {
+    res.status(200).json("Image has been uploaded successfully!");
+});
 
-app.use(cors({ origin:"https://blogssphere.netlify.app", credentials: true }));
+// Serving static images
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.use(cors({
+    origin: "https://blogssphere.netlify.app",
+    credentials: true,
+}));
 
 // Database
 const connectDB = async () => {
@@ -32,24 +49,8 @@ const connectDB = async () => {
     }
 };
 
-    // multer image upload
-    const storage=multer.diskStorage({
-        destination:(req,file,fn)=>{
-            fn(null,"images")
-        },
-        filename:(req,file,fn)=>{
-            fn(null,req.body.img)
-            // fn(null, "imageme.jpg")
-        },
-    })
-    const upload=multer({storage:storage})
-    app.post('/api/upload',upload.single("file"),(req,res)=>{
-        res.status(200).json("Image has been uploaded successfully!")
-    })
-
 // Connect to the database before starting the server
 connectDB().then(() => {
-    
     app.get('/', (req, res) => {
         res.send('api connected');
     });
@@ -66,7 +67,6 @@ connectDB().then(() => {
         console.log(`App is running on port ${PORT}`);
     });
 });
-
 
 
 // const express = require('express');
