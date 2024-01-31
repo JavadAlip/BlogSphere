b88458c5a8ebc387c91efef75898d8e42ad3b73e
JavadAlip <javadalipp911@gmail.com>
2024-02-01 00:31:42 +0530

    url.js add and delete

---
+++ backend/url.js
@@ -0,0 +1,2 @@
+export const VITE_URL="https://blogsphere-backend.onrender.com"
+export const VITE_IMGFOLDER="https://blogsphere-backend.onrender.com/images/"
--- frontend/src/url.js
+++
@@ -1,5 +0,0 @@
-export const VITE_URL="https://blogsphere-backend.onrender.com"
-export const VITE_IMGFOLDER="https://blogsphere-backend.onrender.com/images/"
-
-// export const VITE_URL="http://localhost:10000/"
-// export const VITE_IMGFOLDER="http://localhost:10000/images/"
